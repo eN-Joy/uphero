3bb46717fdc69af0d42911e844461c13376233e0
eN_Joy <jindan@gmail.com>
2014-01-20 18:19:10 -0600

    Modified insert.py

--- insert.py
+++ insert.py
@@ -14,18 +14,23 @@ json_data = open('short.json')
 data = json.load(json_data)
 
 
-for article in data:
-    title = 'I am the author'
-    originator = article['poster']
-    pub_date = article['pdate']
-    content = article['content']
-    category_id = 1
-    link = 'what'
-    media = 'why'
+# for article in data:
+#     # title = article['article']
+#     title = article['article'][0]
+#     originator = article['poster']
+#     pub_date = article['pdate']
+#     content = article['content']
+#     category_id = 1
+#     link = 'link url'
+#     media = 'link url'
     # link = article['post_url']
     # media = article['urls']
+    
 # for article in data[:5]:
+#     print '### Problem here ###'
 #     print article['title']
+#     print type(article['title']) # <type 'list'>
+#     print type(article['title'][0]) # <type 'unicode'>
 #     print article['poster']
 #     print article['pdate']
 #     print article['content']
@@ -33,10 +38,16 @@ for article in data:
 #     print article['post_url']
 #     print article['urls']
 
-    # query = """INSERT INTO ktv_article (title, originator, pub_date,
-    # content, category_id, link, media) VALUES (%s, %s, %s, %s, %s, %s, %s);"""
-    # record = (title, originator, pub_date, content, category_id, link, media)
-    # cursor.execute(query, record)
+    query = """INSERT INTO ktv_article (title, originator, pub_date,
+    content, category_id, link, media) VALUES (%s, %s, %s, %s, %s, %s, %s);"""
+    record = (title, originator, pub_date, content, category_id, link, media)
+    cursor.execute(query, record)
     # conn.commit()
     conn.close()
+
+
+
+
+
+
 
